688f9703ce5c41b44ff8a28fae68ea1584108083
tianyel <48260800+tianyel@users.noreply.github.com>
2019-08-21 15:07:27 -0400

    Add files via upload

---
+++ hasl_para_proc_state.m
@@ -0,0 +1,17 @@
+function asl_para = hasl_para_proc_state(asl_para)
+    
+    if asl_para.PLD_Num == 1
+        
+        asl_para.State_Num = 2;
+        
+    elseif asl_para.PLD_Num == 3
+        
+        asl_para.State_Num = 5;
+        
+    elseif asl_para.PLD_Num == 7
+        
+        asl_para.State_Num = 9;
+        
+    end
+    
+end
