1395abb0f69ed7b241afe6cec1c9abef35db2224
tianyel <48260800+tianyel@users.noreply.github.com>
2019-09-04 10:48:15 -0400

    Add files via upload

---
+++ Step1_create_masks.m
@@ -0,0 +1,32 @@
+%--------------------------------------------------------------------------------------------------
+% Step 1
+% Prep: Brain extraction from MPRAGE to get 'MPRAGE_brain.nii'
+% Purpose: Realign all phases in ASL to M0
+%          create resampled brainmask
+%--------------------------------------------------------------------------------------------------        
+
+subj_folder = '/Users/tianyelin/Desktop/code_github/test_data/S02_20190624'
+
+ASL_path = fullfile(subj_folder, 'ASL/ASL.nii');
+M0_path = fullfile(subj_folder, 'M0/M0.nii');
+
+% realign all ASL phases to M0
+spm_realign_hasl(M0_path, ASL_path);
+
+% set path
+rM0_path = fullfile(subj_folder, 'M0/rM0.nii,1');
+skullstrippedbrain_path = fullfile(subj_folder,'MPRAGE/MPRAGE_brain.nii');
+
+% skullstrippedbrain resliced to rM0
+spm_coreg_reslice({rM0_path},{skullstrippedbrain_path},{''}); 
+
+% save skullstripped mask
+rskullstrippedbrain_path = fullfile(subj_folder,'MPRAGE/rMPRAGE_brain.nii');
+rskullstrippedbrain_img = nii_load_dimg(rskullstrippedbrain_path);
+
+rskullstrippedbrain_img(rskullstrippedbrain_img>0)=1;
+rskullstrippedbrain_img(rskullstrippedbrain_img<=0)=0;
+
+nii = load_nii(fullfile(subj_folder, 'M0/rM0.nii'),[1]);
+nii.img = rskullstrippedbrain_img;
+save_nii(nii, fullfile(subj_folder,'MPRAGE/brain_mask.nii'))
