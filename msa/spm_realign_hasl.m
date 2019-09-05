4af38413f8a2be273e78d6a619408643171aadf7
Will Tackett <will.s.tackett@gmail.com>
2019-09-05 17:16:07 -0400

    move program to msa directory

---
+++ msa/spm_realign_hasl.m
@@ -0,0 +1,35 @@
+function spm_realign_hasl(M0_path, ASL_path, asl_phase_num)
+
+realign_data = {[M0_path,',1']
+                [M0_path,',2']};
+for i = 1:asl_phase_num  
+    f = fullfile([ASL_path, ',', num2str(i)]);
+    realign_data{i+2,1} = f;
+end
+
+clear matlabbatch
+spm('defaults','fmri');
+spm_jobman('initcfg');
+
+matlabbatch{1}.spm.spatial.realign.estwrite.data = {
+                                                    realign_data
+                                                    }';
+
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.quality = 0.9;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.sep = 4;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.fwhm = 5;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.rtm = 1;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.interp = 2;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.weight = '';
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.which = [2 1];
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.interp = 4;
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.mask = 1;
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.prefix = 'r';
+
+spm_jobman('run',matlabbatch);
+clear matlabbatch
+
+end
+
