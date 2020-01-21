0dfe7f7fc1c0127f1f8fd4646e59ab81f6ab1e56
Will Tackett <will.s.tackett@gmail.com>
2020-01-21 14:27:00 -0500

    accomodate only-baseline TT studies (most going fwd)

--- src/full_analysis.m
+++ src/full_analysis.m
@@ -1,4 +1,4 @@
-function []=full_analysis(aslpath, m0path, mpragepath, outputdir)
+function []=full_analysis(aslpath, m0path, mpragepath, hyperC02, stimulus_start, outputdir)
 
 %--------------------------------------------------------------------------------------------------
 % Step 1
@@ -8,11 +8,13 @@ function []=full_analysis(aslpath, m0path, mpragepath, outputdir)
 %--------------------------------------------------------------------------------------------------
 fprintf("Starting Step 1")
 subj_folder = outputdir; % data dir
+stimulus_start = str2double(stimulus_start);
+hyperC02 = logical(str2double(hyperC02));
+% hyperC02_bool = logical(hyperC02);
 
 [~, asl_name, asl_ext] = fileparts(aslpath);
 [~, m0_name, m0_ext] = fileparts(m0path);
 [~, mprage_name, mprage_ext] = fileparts(mpragepath);
-
 
 asl_img = nii_load_dimg(aslpath);
 asl_phase_num = size(asl_img, 4);
@@ -104,8 +106,7 @@ save_nii(nii, fullfile(subj_folder,'MPRAGE/brain_mask.nii'))
 %         Calculate CVR, save change and change% nii
 %--------------------------------------------------------------------------------------------------
 
-normalCO2_state = [1:45];
-hyperCO2_state = [48:60];  % ASL phase numbers
+
 
 hasl_asl_filename = strcat('r', asl_name, asl_ext);
 hasl_m0_filename = strcat('r', m0_name, m0_ext);
@@ -125,7 +126,6 @@ asl_para.PLD = 1;
 asl_para.PLD_Num = 3;
 asl_para.PLD_Lin = 1;
 asl_para.T1b = 1.65;
-
 asl_para = hasl_para_proc_state(asl_para);
 asl_para = hasl_para_proc_ld_pld(asl_para);
 
@@ -141,85 +141,116 @@ phase_num = size(asl_img, 4);
 state_num = asl_para.State_Num;
 loop_num = phase_num./state_num;
 
-% %
-normalCO2stat_img = asl_img(:,:,:,normalCO2_state);
-hyperCO2stat_img = asl_img(:,:,:,hyperCO2_state);
-
-% % decode hasl
-normalCO2_hasl_img = hasl_anymean(normalCO2stat_img, asl_para, normalCO2_state, loop_num);
-normalCO2_hasl_pw = hasl_decode(normalCO2_hasl_img, asl_para);
-
-hyperCO2_hasl_img = hasl_anymean(hyperCO2stat_img, asl_para, hyperCO2_state, loop_num);
-hyperCO2_hasl_pw = hasl_decode(hyperCO2_hasl_img, asl_para);
-
-% %
 nii = load_nii(hasl_m0_path);
 
-% %  CBF,Transit time,ttCBF calculation in different CO2 state
-normalCO2cbf_img = hasl_cbf(normalCO2_hasl_pw, m0_img, brainmsk, asl_para);
-normalCO2cbf_img = hasl_filter_apply(normalCO2cbf_img, brainmsk, smooth_kernel);
 
-hyperCO2cbf_img = hasl_cbf(hyperCO2_hasl_pw, m0_img, brainmsk, asl_para);
-hyperCO2cbf_img = hasl_filter_apply(hyperCO2cbf_img, brainmsk, smooth_kernel);
+% % % % %
+if ~hyperC02
+    state = 1:phase_num;
+    hasl_img = hasl_anymean(asl_img, asl_para, state, loop_num);
+    hasl_pw = hasl_decode(hasl_img, asl_para);
+    cbf_img = hasl_cbf(hasl_pw, m0_img, brainmsk, asl_para);
+    cbf_img = hasl_filter_apply(cbf_img, brainmsk, smooth_kernel);
+    tt_img = hasl_calc_tt(hasl_pw, brainmsk, asl_para);
+    tt_img = hasl_filter_apply(tt_img, brainmsk, smooth_kernel);
+    ttccbf_img = hasl_ttccbf(hasl_pw, m0_img, tt_img, brainmsk, asl_para);
+    
+    % % save nii for CBF, Transit Time and ttCBF
+    nii.hdr.dime.dim(5)=1;
+    nii.img = cbf_img(:,:,:,4);
+    save_nii(nii, fullfile(subj_folder, 'cbf.nii'));
 
-normalCO2tt_img = hasl_calc_tt(normalCO2_hasl_pw, brainmsk, asl_para);
-normalCO2tt_img = hasl_filter_apply(normalCO2tt_img, brainmsk, smooth_kernel);
+    nii.hdr.dime.dim(5)=1;
+    nii.img = ttccbf_img(:,:,:,4);
+    save_nii(nii, fullfile(subj_folder, 'ttcbf.nii'));
 
-hyperCO2tt_img = hasl_calc_tt(hyperCO2_hasl_pw, brainmsk, asl_para);
-hyperCO2tt_img = hasl_filter_apply(hyperCO2tt_img, brainmsk, smooth_kernel);
+    tt_img = tt_img.*1000; % *1000:change unit from 's' to 'ms'
+    nii.hdr.dime.dim(5)=1;
+    nii.img = tt_img;
+    save_nii(nii, fullfile(subj_folder,'tt.nii'));
+    
+else
+    
+    normalCO2_state = 1:stimulus_start-3;
+    hyperCO2_state = stimulus_start:phase_num;  % ASL phase numbers
 
-normalCO2ttccbf_img = hasl_ttccbf(normalCO2_hasl_pw, m0_img, normalCO2tt_img, brainmsk, asl_para);
-normalCO2ttccbf_img = hasl_filter_apply(normalCO2ttccbf_img, brainmsk, smooth_kernel);
+    % %
+    normalCO2stat_img = asl_img(:,:,:,normalCO2_state);
+    hyperCO2stat_img = asl_img(:,:,:,hyperCO2_state);
 
-hyperCO2ttccbf_img = hasl_ttccbf(hyperCO2_hasl_pw, m0_img, hyperCO2tt_img, brainmsk, asl_para);
-hyperCO2ttccbf_img = hasl_filter_apply(hyperCO2ttccbf_img, brainmsk, smooth_kernel);
+    % % decode hasl
+    normalCO2_hasl_img = hasl_anymean(normalCO2stat_img, asl_para, normalCO2_state, loop_num);
+    normalCO2_hasl_pw = hasl_decode(normalCO2_hasl_img, asl_para);
 
-% % save nii for normalCO2(BL) and hyperCO2(HC) state CBF, Transit Time and ttCBF
-nii.hdr.dime.dim(5)=1;
-nii.img = normalCO2cbf_img(:,:,:,4);
-save_nii(nii, fullfile(subj_folder, 'normalCO2_cbf.nii'));
-nii.hdr.dime.dim(5)=1;
-nii.img = hyperCO2cbf_img(:,:,:,4);
-save_nii(nii, fullfile(subj_folder, 'hyperCO2_cbf.nii'));
+    hyperCO2_hasl_img = hasl_anymean(hyperCO2stat_img, asl_para, hyperCO2_state, loop_num);
+    hyperCO2_hasl_pw = hasl_decode(hyperCO2_hasl_img, asl_para);
 
-nii.hdr.dime.dim(5)=1;
-nii.img = normalCO2ttccbf_img(:,:,:,4);
-save_nii(nii, fullfile(subj_folder, 'normalCO2_ttcbf.nii'));
-nii.hdr.dime.dim(5)=1;
-nii.img = hyperCO2ttccbf_img(:,:,:,4);
-save_nii(nii, fullfile(subj_folder, 'hyperCO2_ttcbf.nii'));
+    % %  CBF,Transit time,ttCBF calculation in different CO2 state
+    normalCO2cbf_img = hasl_cbf(normalCO2_hasl_pw, m0_img, brainmsk, asl_para);
+    normalCO2cbf_img = hasl_filter_apply(normalCO2cbf_img, brainmsk, smooth_kernel);
 
-normalCO2tt_img = normalCO2tt_img.*1000; % *1000:change unit from 's' to 'ms'
-nii.hdr.dime.dim(5)=1;
-nii.img = normalCO2tt_img;
-save_nii(nii, fullfile(subj_folder,'normalCO2_tt.nii'));
-hyperCO2tt_img = hyperCO2tt_img.*1000;
-nii.hdr.dime.dim(5)=1;
-nii.img = hyperCO2tt_img;
-save_nii(nii, fullfile(subj_folder,'hyperCO2_tt.nii'));
+    hyperCO2cbf_img = hasl_cbf(hyperCO2_hasl_pw, m0_img, brainmsk, asl_para);
+    hyperCO2cbf_img = hasl_filter_apply(hyperCO2cbf_img, brainmsk, smooth_kernel);
+
+    normalCO2tt_img = hasl_calc_tt(normalCO2_hasl_pw, brainmsk, asl_para);
+    normalCO2tt_img = hasl_filter_apply(normalCO2tt_img, brainmsk, smooth_kernel);
+
+    hyperCO2tt_img = hasl_calc_tt(hyperCO2_hasl_pw, brainmsk, asl_para);
+    hyperCO2tt_img = hasl_filter_apply(hyperCO2tt_img, brainmsk, smooth_kernel);
+
+    normalCO2ttccbf_img = hasl_ttccbf(normalCO2_hasl_pw, m0_img, normalCO2tt_img, brainmsk, asl_para);
+    normalCO2ttccbf_img = hasl_filter_apply(normalCO2ttccbf_img, brainmsk, smooth_kernel);
+
+    hyperCO2ttccbf_img = hasl_ttccbf(hyperCO2_hasl_pw, m0_img, hyperCO2tt_img, brainmsk, asl_para);
+    hyperCO2ttccbf_img = hasl_filter_apply(hyperCO2ttccbf_img, brainmsk, smooth_kernel);
+
+    % % save nii for normalCO2(BL) and hyperCO2(HC) state CBF, Transit Time and ttCBF
+    nii.hdr.dime.dim(5)=1;
+    nii.img = normalCO2cbf_img(:,:,:,4);
+    save_nii(nii, fullfile(subj_folder, 'normalCO2_cbf.nii'));
+    nii.hdr.dime.dim(5)=1;
+    nii.img = hyperCO2cbf_img(:,:,:,4);
+    save_nii(nii, fullfile(subj_folder, 'hyperCO2_cbf.nii'));
+
+    nii.hdr.dime.dim(5)=1;
+    nii.img = normalCO2ttccbf_img(:,:,:,4);
+    save_nii(nii, fullfile(subj_folder, 'normalCO2_ttcbf.nii'));
+    nii.hdr.dime.dim(5)=1;
+    nii.img = hyperCO2ttccbf_img(:,:,:,4);
+    save_nii(nii, fullfile(subj_folder, 'hyperCO2_ttcbf.nii'));
+
+    normalCO2tt_img = normalCO2tt_img.*1000; % *1000:change unit from 's' to 'ms'
+    nii.hdr.dime.dim(5)=1;
+    nii.img = normalCO2tt_img;
+    save_nii(nii, fullfile(subj_folder,'normalCO2_tt.nii'));
+    hyperCO2tt_img = hyperCO2tt_img.*1000;
+    nii.hdr.dime.dim(5)=1;
+    nii.img = hyperCO2tt_img;
+    save_nii(nii, fullfile(subj_folder,'hyperCO2_tt.nii'));
 
 
-% % save change and change% of CBF, tt and ttCBF between BL and HC
-diff_cbf_img = hyperCO2cbf_img(:,:,:,4) - normalCO2cbf_img(:,:,:,4);
-nii.img = diff_cbf_img;
-save_nii(nii, fullfile(subj_folder, 'diff_cbf.nii'));
+    % % save change and change% of CBF, tt and ttCBF between BL and HC
+    diff_cbf_img = hyperCO2cbf_img(:,:,:,4) - normalCO2cbf_img(:,:,:,4);
+    nii.img = diff_cbf_img;
+    save_nii(nii, fullfile(subj_folder, 'diff_cbf.nii'));
 
-cbf_ratio_img = (diff_cbf_img./normalCO2cbf_img(:,:,:,4)).*100;
-nii.img = cbf_ratio_img;
-save_nii(nii, fullfile(subj_folder, 'change%_cbf.nii'));
+    cbf_ratio_img = (diff_cbf_img./normalCO2cbf_img(:,:,:,4)).*100;
+    nii.img = cbf_ratio_img;
+    save_nii(nii, fullfile(subj_folder, 'change%_cbf.nii'));
 
-diff_ttccbf_img = hyperCO2ttccbf_img(:,:,:,4) - normalCO2ttccbf_img(:,:,:,4);
-nii.img = diff_ttccbf_img;
-save_nii(nii, fullfile(subj_folder, 'diff_ttcbf.nii'));
+    diff_ttccbf_img = hyperCO2ttccbf_img(:,:,:,4) - normalCO2ttccbf_img(:,:,:,4);
+    nii.img = diff_ttccbf_img;
+    save_nii(nii, fullfile(subj_folder, 'diff_ttcbf.nii'));
 
-ttccbf_ratio_img = (diff_ttccbf_img./normalCO2ttccbf_img(:,:,:,4)).*100;
-nii.img = ttccbf_ratio_img;
-save_nii(nii, fullfile(subj_folder, 'change%_ttcbf.nii'));
+    ttccbf_ratio_img = (diff_ttccbf_img./normalCO2ttccbf_img(:,:,:,4)).*100;
+    nii.img = ttccbf_ratio_img;
+    save_nii(nii, fullfile(subj_folder, 'change%_ttcbf.nii'));
 
-diff_tt_img = normalCO2tt_img - hyperCO2tt_img;
-nii.img = diff_tt_img;
-save_nii(nii, fullfile(subj_folder, 'diff_tt.nii'));
+    diff_tt_img = normalCO2tt_img - hyperCO2tt_img;
+    nii.img = diff_tt_img;
+    save_nii(nii, fullfile(subj_folder, 'diff_tt.nii'));
 
-tt_ratio_img = (diff_tt_img./normalCO2tt_img).*100;
-nii.img = tt_ratio_img;
-save_nii(nii, fullfile(subj_folder, 'change%_tt.nii'));
+    tt_ratio_img = (diff_tt_img./normalCO2tt_img).*100;
+    nii.img = tt_ratio_img;
+    save_nii(nii, fullfile(subj_folder, 'change%_tt.nii'));
+end
